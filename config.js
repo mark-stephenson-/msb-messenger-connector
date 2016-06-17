e858ad1386ba420988950067bcbe725f06aaaf7b
Mark Stephenson <mark.s@evvnt.com>
2016-06-17 14:21:18 +0100

    add config back in to test heroku deploy

---
+++ config.js
@@ -0,0 +1,9 @@
+/** Config */
+
+module.exports = {
+    FB_PAGE_TOKEN:    process.env.FB_PAGE_TOKEN || "EAAXDAmFjWBcBAPwqhzNBZCQsccJQdSliDEN0mWEhNQ0995GkkRhPaFaX8GoEmKQZB0kT8INFNRTnGZAT8UQiOFlQvmn515oT8oAQOjiyVpiagdkyk8ZBhlXuXNZCbSczk1tGuiqFNOfqMRsi0O3zIjZC2CtXiQA6OzDhhtKZC62ZCAZDZD",
+    FB_PAGE_ID:       process.env.FB_PAGE_ID || "1258741280810408",
+    FB_VERIFY_TOKEN:  process.env.FB_VERIFY_TOKEN || "not_a$_secure_as_it_should_be",
+    WIT_TOKEN:        process.env.WIT_TOKEN || "L6IAWY6O5UEK2O7HOCHK5GJL32HDQGOM",
+    PORT:             process.env.PORT || 8445
+};
